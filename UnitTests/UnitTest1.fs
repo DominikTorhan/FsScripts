54451adf03f58d9157002f9896553f0f3d515f0c
Dominik Torhan <dominik.torhan@gmail.com>
2019-06-13 13:43:55 +0200

    test4

--- UnitTests/UnitTest1.fs
+++ UnitTests/UnitTest1.fs
@@ -18,3 +18,17 @@ type TestClass () =
         let str = "abc sdf" 
         let strings = str.Split " " 
         Assert.AreEqual(2, strings.Length)
+         
+    [<Test>]
+    member this.Test3 () =
+        let str = "abc 
+        sdf" 
+        let strings = str.Split "\n" 
+        Assert.AreEqual(2, strings.Length)
+ 
+    [<Test>] 
+    member this.Test4 ()=
+        let seqA = seq ["ab"; "ab"; "bb";] 
+        let seqB = seqA |> Seq.groupBy (fun s-> s)
+        let len = Seq.length seqB
+        Assert.AreEqual(2, len)
