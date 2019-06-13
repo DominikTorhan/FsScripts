07187d63e2da9e11b96ee43db6d325f13394a530
Dominik Torhan <dominik.torhan@gmail.com>
2019-06-13 15:59:27 +0200

    teset7

--- UnitTests/UnitTest1.fs
+++ UnitTests/UnitTest1.fs
@@ -1,6 +1,7 @@
 namespace Tests
 
 open NUnit.Framework
+open System
 
 [<TestClass>]
 type TestClass () =
@@ -15,8 +16,8 @@ type TestClass () =
           
     [<Test>]
     member this.Test2 () =
-        let str = "abc sdf" 
-        let strings = str.Split " " 
+        let str = "abc  sdf  " 
+        let strings = str.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
         Assert.AreEqual(2, strings.Length)
          
     [<Test>]
@@ -31,4 +32,30 @@ type TestClass () =
         let seqA = seq ["ab"; "ab"; "bb";] 
         let seqB = seqA |> Seq.groupBy (fun s-> s)
         let len = Seq.length seqB
+        Assert.AreEqual(2, len) 
+ 
+    [<Test>] 
+    member this.Test5 ()=
+        let seqA = seq ["ab"; "ab"; "bb";] 
+        let len = seqA |> Seq.distinct |> Seq.length
         Assert.AreEqual(2, len)
+
+    [<Test>] 
+    member this.Test6 () =
+        let str = "1line \n 2line\n\n4line" 
+        let seq = str.Split "\n" |> Array.toSeq 
+        let seq' = seq |> Seq.filter (fun s -> s <> "")
+        Assert.AreEqual(3, seq' |> Seq.length)
+ 
+ 
+    [<Test>] 
+    member this.Test7 () =
+        let str = " 1line \n 2line   \n\n4line" 
+        let seq = str.Split "\n" |> Array.toSeq 
+        let seq' = seq |> Seq.filter (fun s -> s <> "") |> Seq.map (fun s -> s.Trim() + " -") 
+        let strNew = seq' |> String.concat " " 
+        Assert.AreEqual("1line - 2line - 4line -", strNew)
+         
+    //[<Test>] 
+    //member this.Test8 () =
+    //    let str = "AAA, bb. BB-aaa    \n bb Aaa bb 1234065(bb) "
